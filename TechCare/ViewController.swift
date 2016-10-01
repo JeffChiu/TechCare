872ae22fb91fda4d3c8d8fbd68d138074be11452
jeff chiu <zhe.chiu@gmail.com>
2016-10-01 14:06:51 +0800

    Initial Commit

---
+++ TechCare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechCare
+//
+//  Created by Chiu Chih-Che on 2016/10/1.
+//  Copyright © 2016年 Jeff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
